3b2fffbcb3ed58c3e5c207463b5976004f6ed772
张新卫 <zhangxinwei@indemind.cn>
2024-01-02 18:42:31 +0800

    Template pyramid matching without rotation

--- tempalte_match/template_match_from_dir.py
+++ tempalte_match/template_match_from_dir.py
@@ -2,11 +2,9 @@ import argparse
 import glob
 import os
 
-from tempalte_match.template_match_pyramid_2 import get_realsense
 from utils import file
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 
 
 def add_text(img,text):
@@ -27,12 +25,12 @@ def add_text(img,text):
     return img
 
 def tempalte_match_one_image(template, image, methods):
-    template = cv2.cvtColor(template,cv2.COLOR_BGR2GRAY)
+    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
     w, h = template.shape[::-1]
     images = []
     for i in range(len(methods)):
         img1 = image.copy()
-        img = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
         method = eval(methods[i])
         # Apply template Matching
         res = cv2.matchTemplate(img, template, method)
@@ -53,7 +51,7 @@ def tempalte_match_one_image(template, image, methods):
         images.append(img1)
         # 进行横向拼接
     result = cv2.hconcat([images[0], images[1]])
-    add_text(result,"Confidence:" + str(round(confidence,2)))
+    add_text(result, "Confidence:" + str(round(confidence, 2)))
     return result
 
 
@@ -69,6 +67,18 @@ def tempalte_match_many_image(template, image, method, threshold):
         cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (255, 255, 0), 2)
     return image
 
+
+# 金字塔下采样
+def ImagePyrDown(image, NumLevels):
+    for i in range(NumLevels):
+        image = cv2.pyrDown(image)  # pyrDown下采样
+    return image
+
+# 金字塔上采样
+def ImagePyrUp(image, NumLevels):
+    for i in range(NumLevels):
+        image = cv2.pyrUp(image)
+    return image
 
 if __name__ == '__main__':
     # Parse command line arguments.
@@ -88,35 +98,30 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     match_methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
                      'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
-    for i in range(0,len(match_methods),2):
-        methods = [match_methods[i], match_methods[i+1]]
+    for i in range(0, len(match_methods), 2):
+        methods = [match_methods[i], match_methods[i + 1]]
         template = cv2.imread(opt.input_template)
         # shuiping:(77,167);miehuoqi:(91,236);yaoshi:(133,54);bin:(116,134)
         # template = cv2.resize(template,(116,134))
-        search = file.Walk(opt.input_match_dir, ["png","jpeg","jpg"])
+        search = file.Walk(opt.input_match_dir, ["png", "jpeg", "jpg"])
         listing = [glob.glob(s)[0] for s in search]
+        if opt.pyramid_match:
+            template = ImagePyrDown(template, 1)
         for im_path in listing:
             directory, filename = os.path.split(im_path)
             match_image = cv2.imread(im_path)
             if match_image is None:
                 raise Exception('Error reading image %s' % im_path)
-            # 是否是金字塔匹配
-            if opt.pyramid_match:
-                if match_methods[i+1] == 'cv2.TM_SQDIFF_NORMED':
-                    res = get_realsense(match_image, template, True)
-                else:
-                    res = get_realsense(match_image, template, False)
+            if not opt.many_match:
+                res = tempalte_match_one_image(template, match_image, methods)
             else:
-                if not opt.many_match:
-                    res = tempalte_match_one_image(template, match_image, methods)
-                else:
-                    res = tempalte_match_many_image(template, match_image, match_methods[1], 0.8)
+                res = tempalte_match_many_image(template, match_image, match_methods[1], 0.8)
             print(f"{im_path}匹配成功！")
             if not opt.no_display:
                 cv2.imshow("res", res)
                 cv2.waitKey()
             if opt.write_dir:
-                save_dir = os.path.join(opt.write_dir,methods[0].split(".")[-1])
+                save_dir = os.path.join(opt.write_dir, methods[0].split(".")[-1])
                 if not os.path.exists(save_dir):
                     os.makedirs(save_dir, exist_ok=True)
                 save_path = os.path.join(save_dir, os.path.splitext(filename)[0] + ".png")
--- tempalte_match/template_match_pyramid_2.py
+++ tempalte_match/template_match_pyramid_2.py
@@ -4,8 +4,8 @@ import time
 
 
 # 图片旋转函数
-def ImageRotate(img, angle):  # img:输入图片；newIm：输出图片；angle：旋转角度(°)
-    height, width = img.shape[:2]  # 输入(H,W,C)，取 H，W 的值
+def ImageRotate(img, angle):
+    height, width = img.shape[:2]
     center = (width // 2, height // 2)  # 绕图片中心进行旋转
     M = cv2.getRotationMatrix2D(center, angle, 1.0)
     image_rotation = cv2.warpAffine(img, M, (width, height))
@@ -19,48 +19,49 @@ def ImagePyrDown(image, NumLevels):
     return image
 
 
-def tm_ccoeff_normed_match(template_image, match_image, start_angle, end_angle, step):
+def tm_ccoeff_normed_match(template_image, match_image, start_angle, end_angle, step,isRotate=False):
     res = cv2.matchTemplate(match_image, template_image, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_indx, max_indx = cv2.minMaxLoc(res)
     location = max_indx
     temp = max_val
     angle = 0
-    for k_angle in range(start_angle, end_angle, step):
-        newIm = ImageRotate(template_image, k_angle)
-        res = cv2.matchTemplate(match_image, newIm, cv2.TM_CCOEFF_NORMED)
-        min_val, max_val, min_indx, max_indx = cv2.minMaxLoc(res)
-        if max_val > temp:
-            location = max_indx
-            temp = max_val
-            angle = k_angle
+    if isRotate:
+        for k_angle in range(start_angle, end_angle, step):
+            template_image = ImageRotate(template_image, k_angle)
+            res = cv2.matchTemplate(match_image, template_image, cv2.TM_CCOEFF_NORMED)
+            min_val, max_val, min_indx, max_indx = cv2.minMaxLoc(res)
+            if max_val > temp:
+                location = max_indx
+                temp = max_val
+                angle = k_angle
     location_x = location[0]
     location_y = location[1]
-    return (-angle, location_x, location_y)
+    return (-angle, location_x, location_y,temp)
 
 
-def tm_sqdiff_normed_match(template_image, match_image, start_angle, end_angle, step):
+def tm_sqdiff_normed_match(template_image, match_image, start_angle, end_angle, step,isRotate=False):
     # 使用matchTemplate对原始灰度图像和图像模板进行匹配
     res = cv2.matchTemplate(match_image, template_image, cv2.TM_SQDIFF_NORMED)
     min_val, max_val, min_indx, max_indx = cv2.minMaxLoc(res)
     location = min_indx
     temp = min_val
     angle = 0  # 当前旋转角度记录为0
-
-    tic = time.time()
-    # 以步长为5进行第一次粗循环匹配
-    for i in range(start_angle, end_angle, step):
-        newIm = ImageRotate(template_image, i)
-        res = cv2.matchTemplate(match_image, newIm, cv2.TM_SQDIFF_NORMED)
-        min_val, max_val, min_indx, max_indx = cv2.minMaxLoc(res)
-        if min_val < temp:
-            location = min_indx
-            temp = min_val
-            angle = i
-    toc = time.time()
-    print('匹配所花时间为：' + str(1000 * (toc - tic)) + 'ms')
+    if isRotate:
+        tic = time.time()
+        # 以步长为5进行第一次粗循环匹配
+        for i in range(start_angle, end_angle, step):
+            template_image = ImageRotate(template_image, i)
+            res = cv2.matchTemplate(match_image, template_image, cv2.TM_SQDIFF_NORMED)
+            min_val, max_val, min_indx, max_indx = cv2.minMaxLoc(res)
+            if min_val < temp:
+                location = min_indx
+                temp = min_val
+                angle = i
+        toc = time.time()
+        print('匹配所花时间为：' + str(1000 * (toc - tic)) + 'ms')
     location_x = location[0]
     location_y = location[1]
-    return (-angle, location_x, location_y)
+    return (-angle, location_x, location_y,1-temp)
 
 
 def match_tempalte_image(template_image, match_image, start_angle, end_angle, step,is_tm_sqdiff_normed):
@@ -68,23 +69,43 @@ def match_tempalte_image(template_image, match_image, start_angle, end_angle, st
         result = tm_ccoeff_normed_match(template_image, match_image, start_angle, end_angle, step)
     else:
         result = tm_sqdiff_normed_match(template_image, match_image, start_angle, end_angle, step)
-    match_point = {'angle': result[0], 'point': (result[1], result[2])}
+    match_point = {'angle': result[0], 'point': (result[1], result[2]),'confidence':result[3]}
     return match_point
 
 
 # 旋转匹配函数（输入参数分别为模板图像、待匹配图像）
 def RatationMatch(modelpicture, searchpicture,is_tm_sqdiff_normed):
-    searchtmp = ImagePyrDown(searchpicture, 3)
-    modeltmp = ImagePyrDown(modelpicture, 3)
+    searchtmp = ImagePyrDown(searchpicture, 1)
+    modeltmp = ImagePyrDown(modelpicture, 1)
     match_point = match_tempalte_image(modeltmp, searchtmp, -180, 180, 5,is_tm_sqdiff_normed)
     return match_point
 
 
+
+def add_text(img,text):
+    # 定义字体类型、大小和颜色
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.8
+    color = (0, 0, 255)
+
+    # 获取文本框的大小
+    (text_width, text_height), _ = cv2.getTextSize(text, font, font_scale, thickness=2)
+
+    # 计算文本框左下角的坐标
+    x = int((img.shape[1] - text_width) / 2)
+    y = text_height + 10
+
+    # 在图像的最上面中间绘制文本
+    cv2.putText(img, text, (x, y), font, font_scale, color, thickness=2)
+    return img
+
+
 # 画图
-def draw_result(src, temp, match_point):
+def draw_result(src, temp, match_point,confidence):
     cv2.rectangle(src, match_point,
                   (match_point[0] + temp.shape[1], match_point[1] + temp.shape[0]),
                   (0, 255, 0), 2)
+    add_text(src, "Confidence:" + str(round(confidence, 2)))
     return src
 
 
@@ -97,7 +118,7 @@ def get_realsense(match_image, template_image,is_tm_sqdiff_normed):
     print('匹配所花时间为：' + str(1000 * (toc - tic)) + 'ms')
     print('匹配的最优区域的起点坐标为：' + str(match_points['point']))
     print('相对旋转角度为：' + str(match_points['angle']))
-    res_image = draw_result(match_image, ModelImage_edge, match_points['point'])
+    res_image = draw_result(match_image, ModelImage_edge, match_points['point'],match_points['confidence'])
     return res_image
 
 
